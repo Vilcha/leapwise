27e7af20e6e2fe37f42632e70653d96036db51aa
Vilcha <vilim.sepl@gmail.com>
2024-08-09 17:07:48 +0200

    updated Swagger specification with short user manual

--- src/main/java/com/vilim/leapwise/config/SwaggerConfig.java
+++ src/main/java/com/vilim/leapwise/config/SwaggerConfig.java
@@ -8,12 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    @SuppressWarnings("checkstyle:LineLength")
+    private static final String API_DESCRIPTION = """
+            REST API for creating and evaluating logical expressions
+            \s
+            STEP 1 - Enter a logical expression containing logical and relational operations. The expression only accepts data fields of the customer model and currently ignores brackets.
+            \s
+            STEP 2 - Enter a JSON object representing a customer together with the ID of a created expression. Fill in the fields with data that corresponds to the types and structure provided in the example.
+            \s
+            RESULT - The app returns 'true' or 'false' if all inputs and their rules are satisfied OR throw an exception.
+            \s""";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Expression Evaluator API")
-                        .description("REST API for creating and evaluating logical expressions")
+                        .description(API_DESCRIPTION)
                         .version("v1.0"));
     }
 }
